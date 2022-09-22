26173a2794c9092bed3090f9a603daac9446fb71
coalman321 <cjtucker321@gmail.com>
2022-09-22 14:34:11 -0400

    get qwidgets working

--- riptide_rviz/src/control_panel.cpp
+++ riptide_rviz/src/control_panel.cpp
@@ -3,21 +3,25 @@
 
 namespace riptide_rviz
 {
-    MissionPanel::MissionPanel(QWidget *parent)
+    MissionPanel::MissionPanel(QWidget *parent) : rviz_common::Panel(parent)
     {
-        std::cout << "hi form riptide rviz control panel" << std::endl;
-        label = new QLabel(parent);
+        // std::cout << "hi from riptide rviz control panel" << std::endl;
+        setFocusPolicy(Qt::ClickFocus);
+
+        label = new QLabel(this);
         label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-        label->setText("first line\nsecond line");
+        label->setText("jbsdfjkndf jkn");
         label->setAlignment(Qt::AlignBottom | Qt::AlignRight);
     }
 
     void MissionPanel::load(const rviz_common::Config &config)
     {
+        rviz_common::Panel::load( config );
     }
 
     void MissionPanel::save(rviz_common::Config config) const
     {
+        rviz_common::Panel::save( config );
     }
 
     bool MissionPanel::event(QEvent *event)
@@ -31,4 +35,4 @@ namespace riptide_rviz
 } // namespace riptide_rviz
 
 #include <pluginlib/class_list_macros.hpp> // NOLINT
-PLUGINLIB_EXPORT_CLASS(riptide_rviz::MissionPanel, rviz_common::Panel)
+PLUGINLIB_EXPORT_CLASS(riptide_rviz::MissionPanel, rviz_common::Panel);
