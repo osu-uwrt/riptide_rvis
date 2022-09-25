6701cd39b36c26d2a67582131d4eb514b98adb1c
coalman321 <cjtucker321@gmail.com>
2022-09-25 03:28:15 -0400

    the bringup section has the ability to find targets and bringup files

--- riptide_rviz/include/riptide_rviz/ControlPanel.hpp
+++ riptide_rviz/include/riptide_rviz/ControlPanel.hpp
@@ -1,6 +1,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 #include <std_srvs/srv/set_bool.hpp>
+#include <launch_msgs/srv/start_launch.hpp>
+#include <launch_msgs/srv/list_launch.hpp>
+#include <launch_msgs/srv/stop_launch.hpp>
 
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <ament_index_cpp/get_package_prefix.hpp>
@@ -19,15 +22,24 @@ namespace riptide_rviz
         virtual void load(const rviz_common::Config &config);
         virtual void save(rviz_common::Config config) const;
 
-        // protected Q_SLOTS:
+    protected Q_SLOTS:
         // QT slots (function callbacks)
-        // void trigger_service(bool msg, std::string service_name);
+        void bringupListRefresh();
+        void handleBringupHost(int selection);
 
     protected:
         bool event(QEvent *event);
 
     private:
         Ui_ControlPanel *uiPanel;
+
+        // ros node for the panel. we have to spawn our own.
+        rclcpp::Node::SharedPtr nodeHandle;
+
+        rclcpp::Client<launch_msgs::srv::StartLaunch>::SharedPtr bringupStartClient;
+        rclcpp::Client<launch_msgs::srv::ListLaunch>::SharedPtr bringupListClient;
+        rclcpp::Client<launch_msgs::srv::StopLaunch>::SharedPtr bringupStopClient;
+        rclcpp::TimerBase::SharedPtr bringupCheckTimer;
         // void discover_ns();
     };
 
