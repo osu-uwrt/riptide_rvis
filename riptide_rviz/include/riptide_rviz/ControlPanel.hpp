d8bc7b1d613794de49c4e02daa5d77eebe7fcaae
coalman321 <cjtucker321@gmail.com>
2022-10-01 14:25:42 -0400

    begin work on control panel

--- riptide_rviz/include/riptide_rviz/ControlPanel.hpp
+++ riptide_rviz/include/riptide_rviz/ControlPanel.hpp
@@ -2,6 +2,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 
+#include <riptide_msgs2/msg/controller_command.hpp>
+#include <riptide_msgs2/msg/kill_switch_report.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <ament_index_cpp/get_package_prefix.hpp>
 #include <rviz_common/panel.hpp>
@@ -10,8 +14,11 @@
 #include "ui_ControlPanel.h"
 #include <QTimer>
 
+
+
 namespace riptide_rviz
 {
+
     class ControlPanel : public rviz_common::Panel
     {
         Q_OBJECT public : ControlPanel(QWidget *parent = 0);
@@ -22,9 +29,27 @@ namespace riptide_rviz
 
         void onInitialize() override;
 
+        // ROS Subscriber callbacks
+        void odomCallback(const nav_msgs::msg::Odometry & msg);
+
+        // ROS timer callbacks
+        void sendKillMsgTimer();
+
+
     protected Q_SLOTS:
         // QT slots (function callbacks)
-        
+        // slots for handling mode setting of the controller
+        void handleEnable();
+        void handleDisable(); // pressing disable asserts kill and clears command
+        void switchMode(uint8_t mode);
+
+        // slots for controlling the UI
+        void toggleDegrees();
+
+        // slots for sending commands to the vehicle
+        void handleLocalDive();
+        void handleCurrent();
+        void handleCommand();
 
     protected:
         bool event(QEvent *event);
@@ -33,8 +58,27 @@ namespace riptide_rviz
         // UI Panel instance
         Ui_ControlPanel *uiPanel;
 
+        // message for sending commands to the controller
+        riptide_msgs2::msg::ControllerCommand::SharedPtr controllerCommand;
+
+        // internal flags 
+        bool vehicleEnabled = false;
+        bool degreeReadout = true;
+
+        // core stuff for creating and managing a ROS node
         rclcpp::Node::SharedPtr clientNode;
         QTimer * spinTimer;
+
+        // publishers
+        rclcpp::Publisher<riptide_msgs2::msg::ControllerCommand>::SharedPtr ctrlCmdPub;
+        rclcpp::Publisher<riptide_msgs2::msg::KillSwitchReport>::SharedPtr killStatePub;
+
+        // ROS Timers
+        rclcpp::TimerBase::SharedPtr killPubTimer;
+
+        // ROS Subscribers
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odomSub;
+
     };
 
 } // namespace riptide_rviz
--- riptide_rviz/src/ControlPanel.cpp
+++ riptide_rviz/src/ControlPanel.cpp
@@ -1,7 +1,12 @@
 #include "riptide_rviz/ControlPanel.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 #include <chrono>
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+#define ROBOT_NS "/tempest"
 
 namespace riptide_rviz
 {
@@ -12,8 +17,13 @@ namespace riptide_rviz
         uiPanel = new Ui_ControlPanel();
         uiPanel->setupUi(this);
 
+        // create the RCLCPP client
         auto options = rclcpp::NodeOptions().arguments({});
         clientNode = std::make_shared<rclcpp::Node>("riptide_rviz_control", options);
+
+        // create the default message
+        controllerCommand = std::make_shared<riptide_msgs2::msg::ControllerCommand>();
+        controllerCommand->mode = riptide_msgs2::msg::ControllerCommand::DISABLED;
     }
 
     void ControlPanel::onInitialize()
@@ -21,10 +31,42 @@ namespace riptide_rviz
         // create a spin timer
         spinTimer = new QTimer(this);
         connect(spinTimer, &QTimer::timeout, [this](void)
-                { rclcpp::spin_some(clientNode); });
+            { rclcpp::spin_some(clientNode); });
         spinTimer->start(50);
 
+        // make publishers 
+        killStatePub = clientNode->create_publisher<riptide_msgs2::msg::KillSwitchReport>
+            (ROBOT_NS + std::string("/control/software_kill"), rclcpp::SystemDefaultsQoS());
+
+        // make ROS timers
+        killPubTimer = clientNode->create_wall_timer(50ms, std::bind(&ControlPanel::sendKillMsgTimer, this));
+
+        // make ROS Subscribers
+        odomSub = clientNode->create_subscription<nav_msgs::msg::Odometry>(
+            ROBOT_NS + std::string("/odom"), rclcpp::SystemDefaultsQoS(),
+            std::bind(&ControlPanel::odomCallback, this, _1)
+        );
+
+
         // Connect UI signals for controlling the riptide vehicle
+        connect(uiPanel->ctrlEnable, &QPushButton::clicked, [this](void){ handleEnable(); });
+        connect(uiPanel->ctrlDisable, &QPushButton::clicked, [this](void){ handleDisable(); });
+        connect(uiPanel->ctrlDegOrRad, &QPushButton::clicked, [this](void){ toggleDegrees(); });
+
+        // mode seting buttons
+        connect(uiPanel->ctrlModePos, &QPushButton::clicked, 
+            [this](void){ switchMode(riptide_msgs2::msg::ControllerCommand::POSITION); });
+        connect(uiPanel->ctrlModeVel, &QPushButton::clicked, 
+            [this](void){ switchMode(riptide_msgs2::msg::ControllerCommand::VELOCITY); });
+        connect(uiPanel->ctrlModeFFD, &QPushButton::clicked, 
+            [this](void){ switchMode(riptide_msgs2::msg::ControllerCommand::FEEDFORWARD); });
+        // connect(uiPanel->ctrlModeVel, &QPushButton::clicked, 
+        //     [this](void){ switchMode(riptide_msgs2::msg::ControllerCommand::VELOCITY); });
+
+        // command sending buttons
+        connect(uiPanel->ctrlDiveInPlace, &QPushButton::clicked, [this](void){ handleLocalDive(); });
+        connect(uiPanel->ctrlFwdCurrent, &QPushButton::clicked, [this](void){ handleCurrent(); });
+        connect(uiPanel->CtrlSendCmd, &QPushButton::clicked, [this](void){ handleCommand(); });
     }
 
     void ControlPanel::load(const rviz_common::Config &config)
@@ -48,6 +90,94 @@ namespace riptide_rviz
 
         // remove the timers
         delete spinTimer;
+
+        rclcpp::shutdown();
+    }
+
+    // slots for handling mode setting of the controller
+    void ControlPanel::handleEnable(){
+
+    }
+
+    void ControlPanel::handleDisable(){
+
+    }
+
+    void ControlPanel::switchMode(uint8_t mode){
+
+    }
+
+    // slots for sending commands to the vehicle
+    void ControlPanel::handleLocalDive(){
+
+    }
+
+    void ControlPanel::toggleDegrees(){
+        degreeReadout = ! degreeReadout;
+        if(degreeReadout){
+            uiPanel->ctrlDegOrRad->setText("Radians");
+        } else {
+            uiPanel->ctrlDegOrRad->setText("Degrees");
+        }
+    }
+
+    void ControlPanel::handleCurrent(){
+        // take the values from the readouts
+        QString x, y, z, roll, pitch, yaw;
+        x = uiPanel->cmdCurrX->text();
+        y = uiPanel->cmdCurrY->text();
+        z = uiPanel->cmdCurrZ->text();
+        roll = uiPanel->cmdCurrX->text();
+        pitch = uiPanel->cmdCurrX->text();
+        yaw = uiPanel->cmdCurrX->text();
+
+        // take the values and propagate them into the requested values
+        uiPanel->cmdReqX->setText(x);
+        uiPanel->cmdReqY->setText(y);
+        uiPanel->cmdReqZ->setText(z);
+        uiPanel->cmdReqR->setText(roll);
+        uiPanel->cmdReqP->setText(pitch);
+        uiPanel->cmdReqYaw->setText(yaw);
+    }
+
+    void ControlPanel::handleCommand(){
+
+    }
+
+    void ControlPanel::odomCallback(const nav_msgs::msg::Odometry & msg){
+        // parse the quaternion to RPY
+        tf2::Quaternion q(msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+            msg.pose.pose.orientation.z, msg.pose.pose.orientation.w);
+        tf2::Matrix3x3 m(q);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+
+        // convert to degrees if its what we're showing
+        if(degreeReadout){
+            roll *= 180.0 / M_PI;
+            pitch *= 180.0 / M_PI;
+            yaw *= 180.0 / M_PI;
+        }
+
+        // show it to the user
+        uiPanel->cmdCurrR->setText(QString::number(roll, 'f', 2));
+        uiPanel->cmdCurrP->setText(QString::number(pitch, 'f', 2));
+        uiPanel->cmdCurrYaw->setText(QString::number(yaw, 'f', 2));
+
+        uiPanel->cmdCurrX->setText(QString::number(msg.pose.pose.position.x, 'f', 2));
+        uiPanel->cmdCurrY->setText(QString::number(msg.pose.pose.position.y, 'f', 2));
+        uiPanel->cmdCurrZ->setText(QString::number(msg.pose.pose.position.z, 'f', 2));
+    }
+    
+    // ROS timer callbacks
+    void ControlPanel::sendKillMsgTimer(){
+        auto killMsg = riptide_msgs2::msg::KillSwitchReport();
+        killMsg.kill_switch_id = riptide_msgs2::msg::KillSwitchReport::KILL_SWITCH_RQT_CONTROLLER;
+        killMsg.sender_id = "/riptide_rviz_control";
+        killMsg.switch_asserting_kill = !vehicleEnabled;
+        killMsg.switch_needs_update = uiPanel->ctrlRequireKill->isChecked();
+
+        killStatePub->publish(killMsg);
     }
 
 } // namespace riptide_rviz
