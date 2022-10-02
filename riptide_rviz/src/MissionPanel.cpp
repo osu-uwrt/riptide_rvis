40b2cb86bfe11f281837004d8d70f9029c2c12de
Ethan <Harpster.47@osu.edu>
2022-10-02 15:19:18 -0400

    Worked on refresh button

--- riptide_rviz/src/MissionPanel.cpp
+++ riptide_rviz/src/MissionPanel.cpp
@@ -8,6 +8,32 @@ namespace riptide_rviz
 
         uiPanel = new Ui_MissionPanel();
         uiPanel->setupUi(this);
+
+        auto options = rclcpp::NodeOptions().arguments({});
+        clientNode = std::make_shared<rclcpp::Node>("riptide_rviz_mission", options);
+
+    }
+
+    void MissionPanel::onInitialize()
+    {
+        // create a spin timer
+        spinTimer = new QTimer(this);
+        connect(spinTimer, &QTimer::timeout, [this](void)
+                { rclcpp::spin_some(clientNode); });
+        spinTimer->start(50);
+
+        // uiTimer = new QTimer(this);
+        // connect(uiTimer, &QTimer::timeout, [this](void)
+        //         { refreshUI(); });
+        // uiTimer->start(100);
+
+        // Connect UI signals for controlling the riptide vehicle
+        // connect(uiPanel->btStart, &QPushButton::clicked, [this](void)
+        //         { handleEnable(); });
+        // connect(uiPanel->btStop, &QPushButton::clicked, [this](void)
+        //         { handleDisable(); });
+        connect(uiPanel->btRefresh, &QPushButton::clicked, [this](void)
+                { refresh(); });
     }
 
     void MissionPanel::load(const rviz_common::Config &config)
@@ -27,7 +53,36 @@ namespace riptide_rviz
     MissionPanel::~MissionPanel(){
         // master window control removal
         delete uiPanel;
+        delete spinTimer;
     }
+
+    void MissionPanel::refresh()
+    {
+        refreshClient = clientNode->create_client<riptide_msgs::srv::ListTrees>("/tempest/autonomy/list_trees");
+        
+        riptide_msgs::srv::ListTrees::Request::SharedPtr startReq = std::make_shared<riptide_msgs::srv::ListTrees::Request>();
+
+        while (!refreshClient->wait_for_service(100ms))
+                if (!rclcpp::ok())
+                    return;
+        
+        auto refreshFuture = refreshClient->async_send_request();
+        if (rclcpp::spin_until_future_complete(clientNode, refreshFuture) == rclcpp::FutureReturnCode::SUCCESS)
+        {
+
+            auto response = refreshFuture.get();
+            
+            uiPanel->btSelect->clear();
+
+            uiPanel->btSelect->addItem("None Selected");
+            
+            for (auto tree : response->trees)
+            {   
+                // push these into the combo box
+                uiPanel->btSelect->addItem(QString::fromStdString(tree));
+            }
+        }
+    }   
 
 } // namespace riptide_rviz
 
