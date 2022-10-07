adeaccedb158c5db091362532dec513bcab9559a
coalman321 <cjtucker321@gmail.com>
2022-10-07 11:41:23 -0400

    mission panel base feature set works

--- riptide_rviz/include/riptide_rviz/MissionPanel.hpp
+++ riptide_rviz/include/riptide_rviz/MissionPanel.hpp
@@ -1,6 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
+
 #include <riptide_msgs2/srv/list_trees.hpp>
+#include <riptide_msgs2/action/execute_tree.hpp>
 
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <ament_index_cpp/get_package_prefix.hpp>
@@ -12,6 +14,9 @@
 
 namespace riptide_rviz
 {
+    using ExecuteTree = riptide_msgs2::action::ExecuteTree;
+    using GHExecuteTree = rclcpp_action::ClientGoalHandle<ExecuteTree>;
+
     class MissionPanel : public rviz_common::Panel
     {
         Q_OBJECT public : MissionPanel(QWidget *parent = 0);
@@ -19,10 +24,19 @@ namespace riptide_rviz
 
         void load(const rviz_common::Config &config) override;
         void save(rviz_common::Config config) const override;
-        void onInitialize() override; 
+        void onInitialize() override;
     protected Q_SLOTS:
-        //QT slots (function callbacks)
+        // QT slots (function callbacks)
         void refresh();
+        void handleSelectTree(int);
+
+        void startTask();
+        void taskStartCb(const GHExecuteTree::SharedPtr & goalHandle);
+        void cancelTask();
+        void cancelAccept(const action_msgs::srv::CancelGoal::Response::SharedPtr );
+        void taskCompleteCb(const GHExecuteTree::WrappedResult & result);
+        void taskFeedbackCb(GHExecuteTree::SharedPtr goalHandle,
+                            ExecuteTree::Feedback::ConstSharedPtr feedback);
 
     protected:
         bool event(QEvent *event);
@@ -30,8 +44,12 @@ namespace riptide_rviz
     private:
         Ui_MissionPanel *uiPanel;
         rclcpp::Node::SharedPtr clientNode;
-        QTimer * spinTimer;
+        QTimer *spinTimer;
+
+        std::vector<std::string> treeList;
+
         rclcpp::Client<riptide_msgs2::srv::ListTrees>::SharedPtr refreshClient;
+        rclcpp_action::Client<ExecuteTree>::SharedPtr actionServer;
         // void discover_ns();
     };
 
